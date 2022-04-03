89271e70728d03a64971cc2baaad2919da494854
Aishwarya-ram <74194006+Aishwarya-ram@users.noreply.github.com>
2022-04-03 17:29:03 +0530

    Add files via upload

---
+++ 3_Implementations/inc/Simple Calculator.h
@@ -0,0 +1,19 @@
+#ifndef SIMPLE_CALCULATOR_H_INCLUDED
+#define SIMPLE_CALCULATOR_H_INCLUDED
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+#define note "Please enter valid operation"
+void addition();
+void subtraction();
+void multiplication();
+void division();
+void modulus();
+void cube();
+void squareroot();
+void power();
+
+
+
+#endif // SIMPLE_CALCULATOR_H_INCLUDED
